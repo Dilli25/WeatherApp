02a19ccd936d230b21cfe769b5beddeb6148f2bc
dillibabu <dillibabumurali25@gmail.com>
2020-08-18 14:34:03 +0530

    environement file change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
   baseURL:'http://api.openweathermap.org/data/2.5/',
-  APIKEY:'07639de8f90a9e09ffcc723d8bbd1afc'
+  APIKEY:'07639de8f90a9e09ffcc723d8bbd1afc',
+  timeInterval:30000,
+  panelCount:9
 };
